e6a012c711b2a134f876abf3a4719dfd1c2142e0
fahim <fahimmontasirshakil@gmail.com>
2021-02-14 18:58:46 +0600

    2 new features added

--- app.js
+++ app.js
@@ -6,22 +6,15 @@ const sliderBtn = document.getElementById('create-slider');
 const sliderContainer = document.getElementById('sliders');
 // selected image 
 let sliders = [];
-//growing Spinner
-const showSpinner = () => {
-  const spinnerDiv = document.getElementById('growingSpinner');
-  spinnerDiv.classList.remove = 'd-none';
- }
- 
-showSpinner()
-
 // If this key doesn't work
 // Find the name in the url and go to their website
 // to create your own api key
 const KEY = '15674931-a9d714b6e9d654524df198e00&q';
 
+
 // show images 
 const showImages = (images) => {
-  
+  showSpinner()
   imagesArea.style.display = 'block';
   gallery.innerHTML = '';
   // show gallery title
@@ -31,18 +24,19 @@ const showImages = (images) => {
     div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
     div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
     gallery.appendChild(div)
-    
-  })
 
+  })
 }
 
+
 const getImages = (query) => {
+  showSpinner()
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
     .then(data => showImages(data.hits)) //array name spelling fixed
     .catch(err => console.log(err))
-    showSpinner()
 }
+
 
 let slideIndex = 0;
 const selectItem = (event, img) => {
@@ -58,9 +52,12 @@ const selectItem = (event, img) => {
     sliders.splice(index, 1);
   }
 }
-var timer
+
+
+let timer;
 const createSlider = () => {
   // check slider duration
+  // const duration = document.getElementById('duration').value * 1000; converted to second
   const duration = document.getElementById('duration').value;
   if (duration < 0) {
     const warning = document.getElementById('warning');
@@ -84,7 +81,6 @@ const createSlider = () => {
   <span class="prev" onclick="changeItem(-1)"><i class="fas fa-chevron-left"></i></span>
   <span class="next" onclick="changeItem(1)"><i class="fas fa-chevron-right"></i></span>
   `;
-
   sliderContainer.appendChild(prevNext)
   document.querySelector('.main').style.display = 'block';
   // hide image aria
@@ -95,7 +91,8 @@ const createSlider = () => {
     item.className = "slider-item";
     item.innerHTML = `<img class="w-100"
       src="${slide}"
-      alt="">`;
+      alt="">
+      `;
     sliderContainer.appendChild(item)
   })
   changeSlide(0)
@@ -111,12 +108,12 @@ const changeItem = index => {
   changeSlide(slideIndex += index);
 }
 
+
 // change slide item
 const changeSlide = (index) => {
-
   const items = document.querySelectorAll('.slider-item');
   if (index < 0) {
-    slideIndex = items.length - 1
+    slideIndex = items.length - 1;
     index = slideIndex;
   };
 
@@ -126,20 +123,21 @@ const changeSlide = (index) => {
   }
 
   items.forEach(item => {
-    item.style.display = "none"
+    item.style.display = "none";
   })
 
-  items[index].style.display = "block"
+  items[index].style.display = "block";
 }
 
 searchBtn.addEventListener('click', function () {
-
   document.querySelector('.main').style.display = 'none';
   clearInterval(timer);
   const search = document.getElementById('search');
   getImages(search.value)
   sliders.length = 0;
 })
+
+
 //enter key added
 sliderBtn.addEventListener('click', function () {
   createSlider()
@@ -149,3 +147,20 @@ document.getElementById("search").addEventListener("keyup", (event) => {
     document.getElementById("search-btn").click();
   }
 });
+
+
+//growing Spinner
+const showSpinner = () => {
+  const spinnerDiv = document.getElementById('growingSpinner');
+  const imgDiv = document.getElementById('allImg');
+  spinnerDiv.classList.toggle('d-none');
+  imgDiv.classList.toggle('d-none');
+}
+
+
+//back to select image button
+const back = () => {
+  imagesArea.style.display = 'block';
+  document.querySelector('.main').style.display = 'none';
+  clearInterval(timer);
+}
